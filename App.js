ff2a676b93138f85bc0570109134498dcde1188c
Windstorm <62821056+wheinrichs@users.noreply.github.com>
2024-06-14 09:37:40 -0400

    Updated with MongoDB changes

--- App.js
+++ App.js
@@ -5,7 +5,15 @@ import CourseRoutes from "./Kanbas/Courses/routes.js";
 import cors from "cors"
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from './Kanbas/Assignments/routes.js';
+import mongoose from "mongoose";
+import "dotenv/config";
+import UserRoutes from "./Users/routes.js";
 
+
+
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas-su1"
+mongoose.connect(CONNECTION_STRING);
 
 const app = express(); // Sets up the express
 app.use(cors()); // sets up the security so the browser on one server can talk to this server on another port
@@ -16,5 +24,7 @@ Hello(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
+UserRoutes(app);
+
 
 app.listen(process.env.PORT || 4000) // Its either the env port or the local port is env isnt defined
