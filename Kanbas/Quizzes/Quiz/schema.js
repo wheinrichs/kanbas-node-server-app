25c698d6a9025cb9d470754ff30c132d2625cbdb
Windstorm <62821056+wheinrichs@users.noreply.github.com>
2024-06-18 14:01:34 -0400

    Updated schema with defaults

--- Kanbas/Quizzes/Quiz/schema.js
+++ Kanbas/Quizzes/Quiz/schema.js
@@ -4,7 +4,20 @@ const quizzes = new mongoose.Schema({
     points: String,
     courseID: {type: String, required: true},
     available_date: Date,
-    due_date: Date
+    due_date: Date,
+    instructions: String,
+    type: {type: String, default: "gradedQuiz"},
+    assignmentGroup: {type: String, default: "quizzes"},
+    shuffle: {type: Boolean, default: true},
+    time_limit: {type: Boolean, default: true},
+    time: {type: String, default: "20"},
+    attempts: {type: Boolean, default: false},
+    until_date: Date,
+    show_correct_answers: {type: Boolean},
+    one_at_a_time: {type: Boolean, default: true},
+    webcam: {type: Boolean, default: false},
+    lock_after: {type: Boolean, default: false},
+    access_code: String
 },
 {collection: "quizzes"}
 );
