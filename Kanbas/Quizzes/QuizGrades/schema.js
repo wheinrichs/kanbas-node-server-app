a3fa5b94857e481889afb13e5d38a32e74b1c605
Windstorm <62821056+wheinrichs@users.noreply.github.com>
2024-06-25 14:08:05 -0400

    Added the user choices field to each grade

--- Kanbas/Quizzes/QuizGrades/schema.js
+++ Kanbas/Quizzes/QuizGrades/schema.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose"
 const quizGrades = new mongoose.Schema({
     userID: { type: String, required: true },
     quizID: { type: String, required: true },
+    studentChoices: { type: mongoose.Schema.Types.Mixed },
     grade: Number
 },
 {collection: "quiz_grades"}
